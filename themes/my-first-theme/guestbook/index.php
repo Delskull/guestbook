44830cb9dd29bf7db9162884e29e33f912e0ebb1
Delskull <Deluta1989@gmail.com>
2026-09-06 12:47:35 +0300

    feat: add cards

--- themes/my-first-theme/guestbook/index.php
+++ themes/my-first-theme/guestbook/index.php
@@ -51,12 +51,88 @@
     </div>
     <form action="" class="mb-3">
         <div class="form-floating">
-            <textarea class="form-control "  placeholder="Leave a comment here" id="floatingTextarea" style="height: 100px"></textarea>
+            <textarea class="form-control " placeholder="Leave a comment here" id="floatingTextarea"
+                      style="height: 100px"></textarea>
             <label for="floatingTextarea">Comments</label>
         </div>
         <button type="submit" class="btn btn-primary mt-3">Send</button>
     </form>
     <hr>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-3">
+            <div class="card-body">
+
+                <div class="d-flex justify-content-between">
+
+                    <h5 class="card-title">User 1</h5>
+                    <p class="message-created"> 2026-09-06 12:11</p>
+                </div>
+
+
+                <div class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card’s content.
+                </div>
+
+                <div class="card-actions mt-2">
+                    <p>
+                        <a href="#">Disable</a> |
+                        <a href="#">Approve</a> |
+                        <a data-bs-toggle="collapse" href="#collapse-1">Edit</a>
+                    </p>
+                    <div class="collapse" id="collapse-1">
+                        <form action="">
+                            <div class="form-floating">
+                                <textarea class="form-control" placeholder="Leave a comment here" id="message-1"
+                                          style="height: 100px">
+Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                                </textarea>
+                                <label for="message-1">Comments</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-3">Save</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="card mb-3">
+                <div class="card-body">
+
+                    <div class="d-flex justify-content-between">
+
+                        <h5 class="card-title">User 2</h5>
+                        <p class="message-created"> 2026-09-07 12:13</p>
+                    </div>
+
+
+                    <div class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card’s content.
+                    </div>
+
+                    <div class="card-actions mt-2">
+                        <p>
+                            <a href="#">Disable</a> |
+                            <a href="#">Approve</a> |
+                            <a data-bs-toggle="collapse" href="#collapse-2">Edit</a>
+                        </p>
+                        <div class="collapse" id="collapse-2">
+                            <form action="">
+                                <div class="form-floating">
+                                <textarea class="form-control" placeholder="Leave a comment here" id="message-2"
+                                          style="height: 100px">
+Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                                </textarea>
+                                    <label for="message-2">Comments</label>
+                                </div>
+                                <button type="submit" class="btn btn-primary mt-3">Save</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
