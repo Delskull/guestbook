cef6cb8bd521fc4912de5d4521a17a63f7cfadf0
Delskull <Deluta1989@gmail.com>
2026-09-09 11:45:38 +0300

    fix: initialize composer and install valitron validator

--- themes/my-first-theme/guestbook/register.php
+++ themes/my-first-theme/guestbook/register.php
@@ -1,13 +1,18 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/incs/db.php';
 require_once __DIR__ . '/incs/functions.php';
 /** @var PDO $db */
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $data = load(['name', 'email', 'password']);
-    dump($data);
+    $v = new Valitron\Validator($data);
+    var_dump($data);
 }
+
 
 ?>
 <?php
